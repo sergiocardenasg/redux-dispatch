55a87d5478966af5728f2700c2f0901fc6f4a3ff
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-07 16:52:19 -0600

    Revert "solved"
    This reverts commit cb6e7002dbcd69ac83a1d283cc8581cfd95a2581.

--- js/reducer.js
+++ js/reducer.js
@@ -1,22 +1,13 @@
-let state = { count: 0 };
-
-function changeState(state, action) {
+function changeState(state, action){
   switch (action.type) {
-    case "INCREASE_COUNT":
-      return { count: state.count + 1 };
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
     default:
       return state;
   }
 }
 
-function render() {
-  document.body.textContent = state.count;
-}
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
 
-function dispatch(action) {
-  state = changeState(state, action);
-  render();
-}
-
-dispatch({ type: "INCREASE_COUNT" });
-dispatch({ type: "INCREASE_COUNT" });
+changeState(state, action)
